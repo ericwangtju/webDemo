2186305aa65abc04ef618f9376b2c4b116a83aa5
Eric Wang <710035555@qq.com>
2021-09-22 16:32:51 +0800

    完成主页功能

---
+++ 前后端交互阶段/大事件管理后台/assets/js/index.js
@@ -0,0 +1,45 @@
+$(function () {
+  getUserInfo()
+
+  $('#btnLogout').on('click', function () {
+    layui.layer.confirm('是否退出', { icon: 3, title: '提示' }, function (index) {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+      layui.layer.close(index)
+    })
+  })
+})
+
+// 获取用户的基本信息
+function getUserInfo () {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    Headers: {
+      Authorization: localStorage.getItem('token') || ''
+    },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败')
+      }
+      renderAvatar(res.data)
+
+    }
+  })
+}
+
+function renderAvatar (user) {
+  var name = user.nickname || user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic)
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase
+    $('.text-avatar').html(first).show()
+
+  }
+}
