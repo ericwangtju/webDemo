0d64d1e64dd0c26b67acd22b1da9439cb61524bf
Eric Wang <710035555@qq.com>
2021-09-26 23:44:05 +0800

    npm

---
+++ 前后端交互阶段/Node/npm/04.test.js
@@ -0,0 +1,4 @@
+var utils = require('./web-util/index')
+const dtStr = utils.dateFormat(new Date())
+
+console.log(dtStr);
---
+++ 前后端交互阶段/Node/npm/web-util/index.js
@@ -0,0 +1,41 @@
+// 定义格式化时间的函数
+function dateFormat (dateStr) {
+  const dt = new Date(dateStr)
+
+  const y = dt.getFullYear()
+  const m = padZero(dt.getMonth() + 1)
+  const d = padZero(dt.getDate())
+
+  const hh = padZero(dt.getHours())
+  const mm = padZero(dt.getMinutes())
+  const ss = padZero(dt.getSeconds())
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+}
+
+// 定义一个补零的函数
+function padZero (n) {
+  return n > 9 ? n : '0' + n
+}
+
+/ 定义转义 HTML 字符的函数
+function htmlEscape (htmlstr) {
+  return htmlstr.replace(/<|>|"|&/g, match => {
+    switch (match) {
+      case '<':
+        return '&lt;'
+      case '>':
+        return '&gt;'
+      case '"':
+        return '&quot;'
+      case '&':
+        return '&amp;'
+    }
+  })
+}
+
+
+module.exports = {
+  dateFormat,
+  htmlEscape
+}
