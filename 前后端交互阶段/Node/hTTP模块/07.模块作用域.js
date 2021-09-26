043e0df34e83d84b81f27232b40ddc1c74649805
Eric Wang <710035555@qq.com>
2021-09-26 23:44:15 +0800

    npm

---
+++ 前后端交互阶段/Node/hTTP模块/07.模块作用域.js
@@ -0,0 +1,4 @@
+const custom = '章三'
+function sayHello () {
+  console.log('大家好，我是' + username)
+}
---
+++ 前后端交互阶段/Node/hTTP模块/08.test.js
@@ -0,0 +1,5 @@
+// const custom = require('./07.模块作用域')
+// console.log(custom);
+
+const exp = require('./09.moduls.exports对象')
+console.log(exp);
